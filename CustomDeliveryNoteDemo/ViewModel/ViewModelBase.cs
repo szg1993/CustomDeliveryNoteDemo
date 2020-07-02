ea11a5a8bd79c579e7778c364ecc84fb6def68ab
szg1993 <szg1993@gmail.com>
2020-07-02 10:50:09 +0200

    Creating the ViewModelBase.

---
+++ CustomDeliveryNoteDemo/ViewModel/ViewModelBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Needed because of the dispose pattern.
+        /// </summary>
+        private bool isDisposed;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Check if the property is exists.
+        /// </summary>
+        /// <param name="propertyName_in"></param>
+        protected void VerifyPropertyName(string propertyName_in)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName_in] == null)
+            {
+                throw new Exception(String.Format("Nincs ilyen nevű tulajdonság: {0} !", propertyName_in));
+            }
+        }
+
+        /// <summary>
+        /// Inform the UI if something changed.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            VerifyPropertyName(propertyName);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Dispose
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // TODO: dispose managed state (managed objects).
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+            // TODO: set large fields to null.
+
+            isDisposed = true;
+        }
+
+        #endregion
+    }
+}
