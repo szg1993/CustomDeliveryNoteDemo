2d6fdb531e751c79bdc526dd1970821732cc50d2
Gábor Sztárcsevics <szg1993@gmail.com>
2020-07-21 13:04:00 +0200

    Pulled from server.

--- CustomDeliveryNoteDemo/CustomDeliveryNoteDemo/obj/Debug/netcoreapp3.1/DeliveryNoteMessageBox.g.i.cs
+++ CustomDeliveryNoteDemo/CustomDeliveryNoteDemo/obj/Debug/netcoreapp3.1/DeliveryNoteMessageBox.g.i.cs
@@ -109,7 +109,7 @@ namespace CustomDeliveryNoteDemo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CustomDeliveryNoteDemo;component/deliverynotemessagebox.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CustomDeliveryNoteDemo;V1.0.0.0;component/deliverynotemessagebox.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\DeliveryNoteMessageBox.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
