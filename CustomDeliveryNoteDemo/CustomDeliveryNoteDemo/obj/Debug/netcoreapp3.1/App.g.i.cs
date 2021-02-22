2bcd49143d0f532151f4e7649a634fdbc7d22fb7
szg1993 <szg1993@gmail.com>
2021-02-22 17:07:55 +0100

    Starting to refactor the code.

--- CustomDeliveryNoteDemo/CustomDeliveryNoteDemo/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ CustomDeliveryNoteDemo/CustomDeliveryNoteDemo/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -47,7 +47,7 @@ namespace CustomDeliveryNoteDemo {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace CustomDeliveryNoteDemo {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/CustomDeliveryNoteDemo;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CustomDeliveryNoteDemo;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace CustomDeliveryNoteDemo {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public static void Main() {
             CustomDeliveryNoteDemo.App app = new CustomDeliveryNoteDemo.App();
             app.InitializeComponent();
